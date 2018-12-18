d0a5e2ed50c6b3b0ae3c8fb7fb8167c6c1a0f90d
heysun0728 <heysun0728@gmail.com>
2018-12-18 22:06:54 +0800

    Add files via upload

---
+++ miner.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.4.24;
+contract Miner{
+    struct Map{
+        uint bet;
+        bool [5][5] Miner;
+        address winner;
+    }
+    Map[] mapList;
+    address private owner;
+
+    modifier onlyOwner {
+        require (msg.sender == owner);
+        _;
+    }
+    
+    constructor() public{
+        owner=msg.sender;
+    }
+    
+    function showRoom(uint roomNum) public view returns(uint,address){
+        return (mapList[roomNum].bet,mapList[roomNum].winner);
+    }
+
+    function getRoomAmount() public view returns(uint){
+        return mapList.length;
+    }
+    
+    function bury(uint x1,uint y1,uint x2,uint y2,uint x3,uint y3) external payable{
+        require(msg.value > 1 ether);
+        Map memory myMap;
+        myMap.winner=msg.sender;
+        myMap.bet=msg.value;
+        myMap.Miner[x1][y1]=true;
+        myMap.Miner[x2][y2]=true;
+        myMap.Miner[x3][y3]=true;
+        mapList.push(myMap);
+    }
+    
+    function playGame(uint roomNum,uint x1,uint y1,uint x2,uint y2,uint x3,uint y3) external payable{
+        require(msg.value >= mapList[roomNum].bet);
+        Map storage m=mapList[roomNum];
+        if((m.Miner[x1][y1]==true) &&(m.Miner[x2][y2]==true) && (m.Miner[x3][y3]=true)) m.winner=msg.sender;
+        distributePrize(roomNum);
+    }
+    
+    function distributePrize(uint roomNum) public{
+        Map storage m=mapList[roomNum];
+        m.winner.transfer(m.bet*2*8/10);
+        owner.transfer(m.bet*2*2/10);
+        delete mapList[roomNum];
+    }
+}
